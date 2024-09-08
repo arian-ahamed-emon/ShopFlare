efe84cfe2a43f1751377ff9aad087dbd52d78831
arian-ahamed-emon <emonoficialchanel@gmail.com>
2024-09-08 21:33:37 +0600

    Updated

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,4 @@
-import 'package:shop_flare/screens/splash_screen.dart';
+import 'package:shop_flare/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -14,192 +14,139 @@ class _LoginScreenState extends State<LoginScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        title: Text('Log in',style: TextStyle(fontSize: 15,color: Colors.black,fontWeight: FontWeight.bold),),
+        title: const Text('Log in',style: TextStyle(fontSize: 15,color: Colors.black,fontWeight: FontWeight.bold),),
       ),
-      body: Column(
-        children: [
-          SizedBox(height: 10,),
-
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 100,
-                width: 365,
-                color: Colors.white54,
-                child: Column(
-                  //crossAxisAlignment: CrossAxisAlignment.center,
-                  //mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Text(
-                            'Welcome back!',
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold
-                            ),),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Text('Login for faster checkout and get updates on your order.')
-                        ],
-                      ),
-                    ),
-
-                  ],
-                ),
-              ),
-            ],
-          ),
-
-          SizedBox(height: 10,),
-
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 10,),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Padding(
                   padding: const EdgeInsets.all(10.0),
                   child: Container(
-                    color: Colors.white,
+                    height: 50,
+                    width: 365,
+                    color: Colors.white54,
+                    child: const Column(
+                      //crossAxisAlignment: CrossAxisAlignment.center,
+                      //mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.all(1.0),
+                          child: Row(
+                            children: [
+                              Text(
+                                'Welcome back!',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold
+                                ),),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(1.0),
+                          child: Row(
+                            children: [
+                              Text('Login for faster checkout and get updates on your order.')
+                            ],
+                          ),
+                        ),
+
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+
+        
+            const Padding(
+              padding: EdgeInsets.all(1.0),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
                     child: Padding(
-                      padding: const EdgeInsets.all(12.0),
+                      padding: EdgeInsets.all(12.0),
                       child: TextField(
+
                         decoration: InputDecoration(
                           hintText: 'Email Address',
                           labelText: 'Email Address',
-                          border:InputBorder.none),
+                          border:OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(2)))),
                         ),
                         //keyboardType: TextInputType.emailAddress,
                       ),
                     ),
-                  ),
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: Container(
-                    color: Colors.white,
+                  Padding(
+                    padding: EdgeInsets.all(1.0),
                     child: Padding(
-                      padding: const EdgeInsets.all(12.0),
+                      padding: EdgeInsets.all(12.0),
                       child: TextField(
                         decoration: InputDecoration(
                           hintText: 'Password',
                           labelText: 'Password',
-                          border:InputBorder.none,
+                          border:OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(2))),
                           //icon: Icon(Icons.remove_red_eye_sharp)
                         ),
                         keyboardType: TextInputType.visiblePassword,
                         ),
                       ),
                     ),
-                  ),
-                ]
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              alignment: Alignment.bottomRight,
-            child:
-                  Text(
-                    'Forgot Password?',
-                    style: TextStyle(
-                      color: Colors.blue,
-                    fontWeight: FontWeight.bold),
-                  )
-            ),
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ElevatedButton(onPressed: (){
-
-                },
-                    style: ButtonStyle(
-                      backgroundColor:WidgetStatePropertyAll(Colors.black),
-                      shape: WidgetStatePropertyAll(LinearBorder(side: BorderSide(color: Colors.black,style: BorderStyle.none)))
-
-                    ),
-                    child: Text('Log in',style: TextStyle(color: Colors.white),)
-                ),
-              )
-            ],
-          ),
-          Text('-Or-'),
-
-       SizedBox(height: 8,),
-
-          //sign in with google
-
-          GestureDetector(
-            onTap: (){},
-            child: Container(
-              height: 40,
-              width: 445,
-              //color: Colors.black,
-              decoration: BoxDecoration(border: Border.symmetric(horizontal: BorderSide(color: Colors.black),vertical: BorderSide(color: Colors.black))),
-              child: Column(
-                children: [
-                  SizedBox(height: 5,),
-                  Row(
-                    children: [
-                      SizedBox(width: 6,),
-                      Image.asset('assets/images/google.png',height: 25,),
-                  SizedBox(width: 118,),
-                  Text('Contiue with Google',style: TextStyle(fontSize: 18,letterSpacing: 1),)
-                    ],
-                  ),
-                ],
+                  ]
               ),
             ),
-          ),
-
-          SizedBox(height: 15,),
-
-          //sign in with Facebook
-
-          GestureDetector(
-            onTap: (){},
-            child: Container(
-              height: 40,
-              width: 445,
-              //color: Colors.black,
-              decoration: BoxDecoration(border: Border.symmetric(horizontal: BorderSide(color: Colors.black),vertical: BorderSide(color: Colors.black))),
-              child: Column(
-                children: [
-                  SizedBox(height: 5,),
-                  Row(
-                    children: [
-                      SizedBox(width: 5,),
-
-                      Image.asset('assets/images/facebook.png',height: 30,),
-                  SizedBox(width: 117,),
-                  Text('Contiue with Facebook',style: TextStyle(fontSize: 18,letterSpacing: 1),)
-                    ],
-                  ),
-                ],
+            Padding(
+              padding: const EdgeInsets.all(7.0),
+              child: Container(
+                alignment: Alignment.bottomRight,
+              child:
+                    const Text(
+                      'Forgot Password?',
+                      style: TextStyle(
+                        color: Colors.blue,
+                      fontWeight: FontWeight.bold),
+                    )
               ),
             ),
-          ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => const HomeScreen(),));
+                  },
+                      style: const ButtonStyle(
+                        backgroundColor:WidgetStatePropertyAll(Colors.black),
+                        shape: WidgetStatePropertyAll(LinearBorder(side: BorderSide(color: Colors.black,style: BorderStyle.none)))
+        
+                      ),
+                      child: const Text('Log in',style: TextStyle(color: Colors.white),)
+                  ),
+                )
+              ],
+            ),
+        
+         const SizedBox(height: 2,),
+        
+            //sign in with google
+            const SizedBox(height: 3,),
+            //sign in with Facebook
+        
 
-          SizedBox(height: 8,),
-
-          Container(
-            child: Padding(
+            const SizedBox(height: 8,),
+        
+            Padding(
               padding: const EdgeInsets.all(8.0),
               child: RichText(
                 textAlign: TextAlign.center,
-                 text: TextSpan(
+                 text: const TextSpan(
                    children: [
                  TextSpan(text : 'By creating account or signing in,you have read and agreed to ',style: TextStyle(color: Colors.black)),
-                     TextSpan(text : 'parivacy policy ',style: TextStyle(color: Colors.blue)),
+                     TextSpan(text : 'privacy policy ',style: TextStyle(color: Colors.blue)),
                      TextSpan(text: 'and ',style: TextStyle(color: Colors.black)),
                      TextSpan(text: 'tram of service',style: TextStyle(color: Colors.blue))
                    ]
@@ -207,30 +154,30 @@ class _LoginScreenState extends State<LoginScreen> {
 
               ),
             ),
-          ),
-          SizedBox(height: 70,),
-
-          Container(
-            height: 100,
-            width: 150,
-           // color: Colors.black,
-            child: Column(
-              children: [
-              RichText(
-                textAlign: TextAlign.center,
-                text: TextSpan(
-                    children: [
-                      TextSpan(text : "Don't have an account?",style: TextStyle(color: Colors.black45)),
-                      TextSpan(text : 'Sign Up Now ',style: TextStyle(color: Colors.blue)),
-                    ]
+            const SizedBox(height: 40,),
+        
+            SizedBox(
+              height: 100,
+              width: 150,
+             // color: Colors.black,
+              child: Column(
+                children: [
+                RichText(
+                  textAlign: TextAlign.center,
+                  text: const TextSpan(
+                      children: [
+                        TextSpan(text : "Don't have an account?",style: TextStyle(color: Colors.black45)),
+                        TextSpan(text : 'Sign Up Now ',style: TextStyle(color: Colors.blue)),
+                      ]
+                  ),
+        
                 ),
-
-              ),
-                ]
-          )
-          )
-        ],
-          )
+                  ]
+            )
+            )
+          ]
+        ),
+            ),
       );
   }
 }
